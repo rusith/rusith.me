0a941e30b7fd75f85cc447f2889dc38cab7e415d
Rusith <rusith@mail.com>
2020-11-11 21:34:20 +0530

    added blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <h1>
+            Rusith.Me Blog Page
+        </h1>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
+import Link from "next/link";
+import React from "react";
 
 export default function Home() {
   return (
       <h1>
           Rusith.Me Home Page
+          
+          <Link href="/blog/" >
+              <a>Blog</a>
+          </Link>
       </h1>
   )
-}
-
-export async function getStaticPaths() {
-    
 }
 
