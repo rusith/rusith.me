a6fe11d4f127c2f48ca2f8a042ed7aec67d14f55
Rusith <rusith@mail.com>
2020-11-15 18:46:22 +0530

    moved release script to sh file again

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+npm install
+export NODE_ENV=production
+git config user.name "Rusith"
+git config user.email "rusith@mail.com"
+git checkout -b release
+git pull origin release
+npm run lint
+npm run build
+npm run export
 shopt -s extglob
 rm -rf !(".git"|"out"|"scripts") 
 rm -rf @(".gitignore"|".github"|".idea"|".vscode") 
