854a959bf3d9ea960c1f74b47e5d528df053ed70
Rusith <rusith@mail.com>
2020-11-15 17:36:13 +0530

    again release changes

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-npm install
-export NODE_ENV=production
-npm run lint
-npm run build
-npm run export
-git config user.name Rusith
-git config user.email rusith@mail.com
-git checkout -b release
-git pull origin release
 rm -rf !(".git"|"out"|"scripts") 
 rm -rf @(".gitignore"|".github"|".idea"|".vscode") 
 cp -a ./out/. ./
