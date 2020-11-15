e6b087e0f485b44ebe374f28f3467608ea9f5ffa
Rusith <rusith@mail.com>
2020-11-15 17:27:11 +0530

    release script changes

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-npm install
-export NODE_ENV=production
-npm run lint
-npm run build
-npm run export
-git config user.name "Rusith"
-git config user.email "rusith@mail.com"
-git checkout -b release
-git pull origin release
 rm -v -rf !(".git"|"out"|"scripts") 
 rm -v -rf @(".gitignore"|".github"|".idea"|".vscode") 
 cp -a ./out/. ./
 rm -rf ./out
-git add .
