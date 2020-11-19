37faca291f231d01cb7486dd189eeff825ff872b
Rusith <rusith@mail.com>
2020-11-19 11:57:01 +0530

    fail when failed

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,9 +4,9 @@ export NODE_ENV=production
 git config user.name "Rusith"
 git config user.email "rusith@mail.com"
 git checkout live
-npm run lint
-npm run build
-npm run export
+npm run lint || { echo 'linting failed' ; exit 1; }
+npm run build || { echo 'build failed' ; exit 1; }
+npm run export  || { echo 'export failed' ; exit 1; }
 shopt -s extglob
 rm -rf !(".git"|"out"|"scripts"|".gitignore"|"sitemap.xml") 
 rm -rf @(".github"|".idea"|".vscode") 
