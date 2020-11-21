f7f5702f79dc4e2f3e19ec35806bbbce3d49cd91
Rusith <rusith@mail.com>
2020-11-21 11:05:21 +0530

    fixed tag page and sitemap

--- modules/app/services/sitemapService.ts
+++ modules/app/services/sitemapService.ts
@@ -11,18 +11,18 @@ export async function createSitemap() {
     const allPages = await getAvailablePageCount()
 
     posts.forEach(post => {
-        paths.push({ url: post.path, lastmod: post.dateModified })
+        paths.push({ url: post.path + "/", lastmod: post.dateModified })
         if(post.oldPath) {
-            paths.push({ url: post.oldPath, lastmod: post.dateModified  })
+            paths.push({ url: post.oldPath + "/", lastmod: post.dateModified  })
         }
     })
 
-    paths = [...paths, ...['/', '/about'].map(c => ({ url: c } as any))]
+    paths = [...paths, ...['/', '/about/'].map(c => ({ url: c } as any))]
 
     if (allPages > 1) {
         _.times(allPages, n => {
             if (n > 0) {
-                paths.push({ url: `/page${n + 1}` })
+                paths.push({ url: `/page${n + 1}/` })
             }
         })
     }
--- modules/blog/components/tag/Tag.tsx
+++ modules/blog/components/tag/Tag.tsx
@@ -8,55 +8,56 @@ import comp from "styles/comp.module.scss"
 import Head from "next/head"
 
 type Props = {
-    tag: string
-    posts: IPostLink[]
-    tags: string[],
-    topTags: string[]
+  tag: string;
+  posts: IPostLink[];
+  tags: string[];
+  topTags: string[];
 }
 
 const Tag: React.FC<Props> = ({ tag, posts, topTags, tags }) => {
-    return (
-        <div className={comp.content}>
-            <Sidebar topTags={topTags}/>
-            <div>
-                <h1>Tag: {tag}</h1>
-                <ul>
-                    {posts.map(post => (
-                        <li>
-                            <Link prefetch={false} href={post.fullUrl}>
-                                <a>{post.title}</a>
-                            </Link>
-                            ({post.date}) <br/>
-                            {post.description}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-
-            <h2>Archive</h2>
-            <div className={styles.tagList}>
-                {tags.map(t => (
-                    <Link href={url + "/tag/" + t} prefetch={false} key={t}>
-                        <a>
-                            <code className="highligher-rouge">
-                                {t}
-                            </code>
-                        </a>
-                    </Link>
-                ))}
-            </div>
-
-            <Head>
-                <title>
-                    Tag - {tag}
-                </title>
-                <meta name="robots" content="noindex,follow" key="robots" />
-                <meta property="og:url" content={url + "/tag/" + tag} key="og_url" />
-                <meta property="canonical" content={url + "/tag/" + tag} key="canonical" />
-            </Head>
+  return (
+    <>
+      <Sidebar topTags={topTags} />
+      <div className={comp.content}>
+        <div>
+          <h1>Tag: {tag}</h1>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.fullUrl}>
+                <Link prefetch={false} href={post.fullUrl}>
+                  <a>{post.title}</a>
+                </Link>
+                ({post.date}) <br />
+                {post.description}
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+
+        <h2>Archive</h2>
+        <div className={styles.tagList}>
+          {tags.map((t) => (
+            <Link href={url + "/tag/" + t} prefetch={false} key={t}>
+              <a>
+                <code className="highligher-rouge">{t}</code>
+              </a>
+            </Link>
+          ))}
+        </div>
+
+        <Head>
+          <title>Tag - {tag}</title>
+          <meta name="robots" content="noindex,follow" key="robots" />
+          <meta property="og:url" content={url + "/tag/" + tag} key="og_url" />
+          <meta
+            property="canonical"
+            content={url + "/tag/" + tag}
+            key="canonical"
+          />
+        </Head>
+      </div>
+    </>
+  )
 }
 
 export default Tag
